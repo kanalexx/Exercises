eb6ad3001344ff3d22c1c2a4a7e4e203f622f111
kanaa <A.Kanunnikov@bssys.com>
2020-04-06 20:23:24 +0300

    Дешифровка аффинного шифра.

--- src/com/company/crypto/basic/substitution/AffineCipher.java
+++ src/com/company/crypto/basic/substitution/AffineCipher.java
@@ -2,15 +2,16 @@ package com.company.crypto.basic.substitution;
 
 import com.company.crypto.basic.alphabet.Alphabet;
 import com.company.crypto.basic.ariphmetic.Coprime;
+import com.company.crypto.basic.ariphmetic.GcdEx;
 
 /**
  * Аффинный шифр
  *
  * <br> Функция шифрования: E(x) = (a*x + b) mod m
- * <br> Функция дешифрования: D(x) = pow(a,-1)(x - b) mod m
+ * <br> Функция дешифрования: D(x) = inverse_a*(x + m - b) mod m
  * <br> где m - размер алфавита; пара a и b — ключ шифра.
  * <br> Значение a должно быть выбрано таким, что a и m — взаимно простые числа.
- *
+ * <br> В дешифровании pow(a,-1) - это число обратное числу а по модулю m.
  * @author Alexander Kanunnikov
  */
 
@@ -20,11 +21,17 @@ public class AffineCipher extends SimpleSubstitutionCipher {
 
     private int key_b;
 
+    private int inverse_a;
+
     public AffineCipher(Alphabet alphabet, int a, int b) {
         super(alphabet);
         this.key_a = a;
         this.key_b = b;
         checkKeyA();
+        // обратное число ищется с помощью расширенного алгоритма Эйлера
+        int x = (new GcdEx(key_a, alphabet.size())).x();
+        // x = (x mod m + m) mod m
+        inverse_a = (x % alphabet.size() + alphabet.size()) % alphabet.size();
     }
 
     /**
@@ -46,7 +53,9 @@ public class AffineCipher extends SimpleSubstitutionCipher {
 
     @Override
     protected String plainSymbol(String cipherSymbol) {
-        return "";
+        // D(x) = inverse_a*(x + m - b) mod m
+        int plainIndex = (inverse_a * (alphabet.index(cipherSymbol) + alphabet.size() - key_b)) % alphabet.size();
+        return alphabet.symbol(plainIndex);
     }
 
 }
