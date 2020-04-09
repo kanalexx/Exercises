fe211e4937bbcbb6ef0e6f135946b98cb9a8bd4a
kanaa <A.Kanunnikov@bssys.com>
2020-04-10 01:23:15 +0300

    Матрица. Седловые точки.

---
+++ test/com/company/mathan/matrix/ArrayTest.java
@@ -0,0 +1,41 @@
+package com.company.mathan.matrix;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Alexander Kanunnikov
+ */
+public class ArrayTest {
+
+    @Test
+    public void indexOfMax() {
+        Array<Integer> array = new Array<>(1, 2);
+        assertEquals(1, array.indexOfMax());
+        //
+        array = new Array<>(2, 1);
+        assertEquals(0, array.indexOfMax());
+        //
+        array = new Array<>(2, 3, 1);
+        assertEquals(1, array.indexOfMax());
+        //
+        array = new Array<>(2, 4, 1, 3);
+        assertEquals(1, array.indexOfMax());
+    }
+
+    @Test
+    public void indexOfMin() {
+        Array<Integer> array = new Array<>(1, 2);
+        assertEquals(0, array.indexOfMin());
+        //
+        array = new Array<>(2, 1);
+        assertEquals(1, array.indexOfMin());
+        //
+        array = new Array<>(2, 1, 3);
+        assertEquals(1, array.indexOfMin());
+        //
+        array = new Array<>(5, 1, 4, 2, 3);
+        assertEquals(1, array.indexOfMin());
+    }
+}
---
+++ test/com/company/mathan/matrix/MatrixTest.java
@@ -0,0 +1,13 @@
+package com.company.mathan.matrix;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Alexander Kanunnikov
+ */
+public class MatrixTest {
+
+
+}
