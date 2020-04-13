451c9aefa516301086cef6ec7a3d0bb9a8d8782a
kanaa <A.Kanunnikov@bssys.com>
2020-04-14 00:12:46 +0300

    Криптоанализ. Частотный анализ.

--- src/com/kanaa/crypto/basic/cryptanalysis/SymbolFreqFile.java
+++ src/com/kanaa/crypto/basic/cryptanalysis/SymbolFreqFile.java
@@ -1,12 +1,15 @@
 package com.kanaa.crypto.basic.cryptanalysis;
 
 import com.kanaa.common.Logger;
+import com.kanaa.common.Pair;
 
 import java.io.*;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.TreeMap;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
 import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
 /**
  * @author Alexander Kanunnikov
@@ -14,17 +17,23 @@ import java.util.regex.Pattern;
 
 public class SymbolFreqFile {
 
-    public static void main(String[] args) {
-        final String textFileName = "C:\\text.txt";
-        final String freqFileName = "C:\\freq.txt";
-        final String symbolMask = "[ А-Я]";
-        //
+    public static final Logger LOGGER = new Logger();
+
+    public static final String resources = System.getProperty("user.dir")+"\\resources\\";
+
+    private final String freqFileName;
+
+    public SymbolFreqFile(String freqFileName) {
+        this.freqFileName = freqFileName;
+    }
+
+    public SymbolFreqFile createFrom(String textFileName, String symbolMask, String charsetName) throws IOException {
         HashMap<String, Integer> countMap = new HashMap<>(100);
         int allCount = 0;
-        Logger logger = new Logger();
-        logger.logf("Чтение файла с текстом %s", textFileName);
-        try (InputStream is = new FileInputStream(textFileName)) {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(is, "Cp1251"));
+        LOGGER.logf("Чтение файла с текстом %s", textFileName);
+        try (InputStream is = new FileInputStream(textFileName);
+             BufferedReader reader = new BufferedReader(new InputStreamReader(is, charsetName))
+        ) {
             int cp;
             Pattern pattern = Pattern.compile(symbolMask);
             while ((cp = reader.read()) != -1) {
@@ -40,29 +49,55 @@ public class SymbolFreqFile {
                     allCount++;
                 }
             }
-        } catch (IOException e) {
-            e.printStackTrace();
         }
-        logger.logf("Найдено %s символов, удовлетворяющих маске", allCount, symbolMask);
+        LOGGER.logf("Найдено %s символов, удовлетворяющих маске \"%s\"", allCount, symbolMask);
         //
-        logger.log("Подсчет частот");
-        TreeMap<Float, String> freqMap = new TreeMap<>();
+        LOGGER.log("Подсчет частот");
+        ArrayList<Pair<String, Float>> freqList = new ArrayList<>();
         if (allCount > 0) {
             for (Map.Entry<String, Integer> entry : countMap.entrySet()) {
-                freqMap.put(entry.getValue().floatValue() / (float) allCount, entry.getKey());
+                freqList.add(new Pair<>(entry.getKey(), entry.getValue().floatValue() / (float) allCount));
             }
         }
-        logger.logf("Сохранение частот в файл %s", freqFileName);
-        try (OutputStream os = new FileOutputStream(freqFileName)) {
-            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os, "Cp1251"));
-            for (Map.Entry<Float, String> entry : freqMap.descendingMap().entrySet()) {
-                writer.write(String.format("%s=%f\n", entry.getValue(), entry.getKey()));
+        //
+        LOGGER.logf("Сохранение частот в файл %s", freqFileName);
+        freqList.sort(Collections.reverseOrder(Comparator.comparing(Pair::getSecond)));
+        try (OutputStream os = new FileOutputStream(freqFileName);
+             BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os, "Cp1251"))
+        ) {
+            for (Pair<String, Float> pair : freqList) {
+                writer.write(pair.toString() + "\n");
             }
-            writer.close();
+        }
+        //
+        LOGGER.log("ГОТОВО!");
+        return this;
+    }
+
+    public static void main(String[] args) {
+        // Создание файла частот символов в тексте на русском языке
+        SymbolFreqFile symbolFreqFile = new SymbolFreqFile(resources+"freq.txt");
+        try {
+            symbolFreqFile.createFrom(resources+"text.txt","[ А-Я]", "Cp1251");
         } catch (IOException e) {
             e.printStackTrace();
         }
-        logger.log("ГОТОВО!");
+        // получение строки из файла с зашифрованным текстом
+        try (Stream<String> stream = Files.lines(Paths.get(resources+"cipher.txt"), Charset.forName("Cp866"))) {
+            StringBuilder sb = new StringBuilder();
+            stream.forEach(sb::append);
+            System.out.println(sb.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // получение файла частот символов в зашифрованном тексте
+        SymbolFreqFile cipherFreqFile = new SymbolFreqFile(resources+"freqCipher.txt");
+        try {
+            cipherFreqFile.createFrom(resources+"cipher.txt", ".", "Cp866");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
 }
