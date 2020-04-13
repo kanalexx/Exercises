38ff3930c710f52fc5f1da1e37a620c11f2d02c2
kanaa <A.Kanunnikov@bssys.com>
2020-04-14 00:06:22 +0300

    Общее. Пара значений.

---
+++ src/com/kanaa/common/Pair.java
@@ -0,0 +1,53 @@
+package com.kanaa.common;
+
+import java.util.Objects;
+
+/**
+ * Пара значений
+ * @author Alexander Kanunnikov
+ */
+
+public class Pair<T, V> {
+    private T first;
+    private V second;
+
+    public Pair(T first, V second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public T getFirst() {
+        return first;
+    }
+
+    public V getSecond() {
+        return second;
+    }
+
+    public void setFirst(T first) {
+        this.first = first;
+    }
+
+    public void setSecond(V second) {
+        this.second = second;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pair<?, ?> pair = (Pair<?, ?>) o;
+        return Objects.equals(first, pair.first) &&
+                Objects.equals(second, pair.second);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(first, second);
+    }
+
+    @Override
+    public String toString() {
+        return first + "=" + second;
+    }
+}
