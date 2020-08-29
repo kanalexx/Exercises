776c78935c3abedd4cbbddf48b16b972bddee5aa
KanAA <A.Kanunnikov@bssys.com>
2020-08-29 11:20:42 +0300

    Матрицы. Инициализация из файла.

--- src/com/kanaa/mathan/matrix/Matrix.java
+++ src/com/kanaa/mathan/matrix/Matrix.java
@@ -1,10 +1,11 @@
 package com.kanaa.mathan.matrix;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
+import com.sun.xml.internal.ws.commons.xmlutil.Converter;
+
+import java.io.*;
+import java.util.*;
 import java.util.function.Consumer;
+import java.util.function.Function;
 
 /**
  * @author Alexander Kanunnikov
@@ -13,24 +14,55 @@ import java.util.function.Consumer;
 public class Matrix<T extends Number & Comparable<T>> {
 
     private List<List<T>> value;
-    private final int rowCount;
-    private final int colCount;
 
     public Matrix(final T[][] value) {
         this.value = new ArrayList<>(value.length);
         for (T[] row : value) {
             this.value.add(Arrays.asList(row));
         }
-        rowCount = this.value.size();
-        colCount = this.value.get(0).size();
     }
 
     public Matrix(List<List<T>> value) {
         this.value = value;
-        rowCount = value.size();
-        colCount = value.get(0).size();
     }
 
+    public Matrix(File file, Function<String, T> converter) throws IOException {
+        this.value = new ArrayList<>();
+        try ( BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            reader.lines().forEach(line -> {
+                String[] row = line.split(" ");
+                List<T> rowList = new ArrayList<>(row.length);
+                for (String item : row) {
+                    rowList.add(converter.apply(item));
+                }
+                this.value.add(rowList);
+            });
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Matrix<?> matrix = (Matrix<?>) o;
+        return value.equals(matrix.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        value.forEach(sb::append);
+        return sb.toString();
+    }
 
     public T value(int row, int col) {
         return value.get(row).get(col);
@@ -54,16 +86,16 @@ public class Matrix<T extends Number & Comparable<T>> {
     }
 
     public List<T> col(int index) {
-        List<T> col = new ArrayList<>(rowCount);
-        for (int i = 0; i < rowCount; i++) {
+        List<T> col = new ArrayList<>(rowCount());
+        for (int i = 0; i < rowCount(); i++) {
             col.add(value(i, index));
         }
         return col;
     }
 
     public Matrix<T> transpose() {
-        List<List<T>> transposed = new ArrayList<>(colCount);
-        for (int i = 0; i < colCount; i++) {
+        List<List<T>> transposed = new ArrayList<>(colCount());
+        for (int i = 0; i < colCount(); i++) {
             transposed.add(col(i));
         }
         return new Matrix<>(transposed);
@@ -71,7 +103,7 @@ public class Matrix<T extends Number & Comparable<T>> {
 
     public Collection<T> saddlePoints() {
         Matrix<T> transposed = this.transpose();
-        ArrayList<T> result = new ArrayList<>(Math.min(rowCount, colCount));
+        ArrayList<T> result = new ArrayList<>(Math.min(rowCount(), colCount()));
         for (List<T> row : value) {
             int minInRow = (new Array<>(row)).indexOfMin();
             List<T> col = transposed.row(minInRow);
@@ -81,6 +113,14 @@ public class Matrix<T extends Number & Comparable<T>> {
             }
         }
         return result;
+    }
+
+    public int rowCount() {
+        return value.size();
+    }
+
+    public int colCount() {
+        return !value.isEmpty() ? value.get(0).size() : 0;
     }
 
 }
--- test/com/kanaa/mathan/matrix/MatrixTest.java
+++ test/com/kanaa/mathan/matrix/MatrixTest.java
@@ -1,9 +1,32 @@
 package com.kanaa.mathan.matrix;
 
+import com.kanaa.Resources;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
 /**
  * @author Alexander Kanunnikov
  */
 public class MatrixTest {
 
+    @Test
+    public void common() {
+        Matrix<Integer> matrix = new Matrix<>(new Integer[][] {{0,1},{1,0},{1,2}});
+        //
+        assertEquals(3, matrix.rowCount());
+        assertEquals(2, matrix.colCount());
+        assertEquals(Arrays.asList(1, 0), matrix.row(1));
+        assertEquals(Arrays.asList(1, 0, 2), matrix.col(1));
+    }
 
+    @Test
+    public void readFromFile() throws IOException {
+        Matrix<Integer> matrixFromFile = new Matrix<>(Resources.file("testMatrix.txt"), Integer::valueOf);
+        Matrix<Integer> matrix = new Matrix<>(new Integer[][] {{0,1},{1,0},{1,2}});
+        assertEquals(matrix, matrixFromFile);
+    }
 }
